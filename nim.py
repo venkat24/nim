28598480d006a1ebf3a73c0d272f44378ef5b395
MasterPabu <venkat24@outlook.com>
2016-07-21 13:13:39 +0000

    AI working

--- nim.py
+++ nim.py
@@ -3,50 +3,68 @@ from string import ascii_uppercase
 import sys
 
 heaps=[]
-numberOfHeaps=5
+numberOfHeaps=3
 maxHeapSize=10
 currentPlayer=True
 
 def displayHeap():
-	print
-	print '  '.join(ascii_uppercase[:numberOfHeaps])
-	print '  '.join(str(x) for x in heaps)
+    print
+    print '  '.join(ascii_uppercase[:numberOfHeaps])
+    print '  '.join(str(x) for x in heaps)
 
 def checkWinState(player):
-	if sum(heaps)==0:
-		print "GAME OVER!"
-		if player:
-			print "You Win!"
-		else:
-			print "I win"
-		sys.exit(0)
+    player = not currentPlayer
+    if sum(heaps)==0:
+        print "GAME OVER!"
+        if player:
+            print "You Win!"
+        else:
+            print "I win"
+        sys.exit(0)
 
 for i in xrange(numberOfHeaps):
-	heaps.append(random.randint(1,maxHeapSize))
+    heaps.append(random.randint(1,maxHeapSize))
 
 def computerMove():
-	nim_sum=reduce(lambda a,b: a^b, heaps)
-	print "{0:b}".format(nim_sum)
+    print "My move : ",
+    global numberOfHeaps
+    nim_sum=reduce(lambda a,b: a^b, heaps)
+    bin_nim=int("{0:b}".format(nim_sum))
+    largest=heaps.index(max(heaps))
+    curr_heap=0
+    for i in xrange(numberOfHeaps):
+        if nim_sum^heaps[i] < heaps[i] :
+            curr_heap=i
+            print ascii_uppercase[i] + " ",
+    if bin_nim==0: #losing position, hope for opponent mistake.
+        heaps[largest]-=random.randint(1,max(heaps))
+    else:
+        prev_val = heaps[curr_heap]
+        heaps[curr_heap]^=nim_sum
+        new_val=heaps[curr_heap]
+        print prev_val - new_val
 
 print "\nWelcome to NIM!"
 while(True):
-	try:
-		displayHeap()
-		if currentPlayer:
-			print "Your move : ",
-		else:
-			print "My move : ",
-			computerMove()
-		userInputHeap,userInputNumber = raw_input().split()
-		userInputNumber=int(userInputNumber)
-		heapIndex=ascii_uppercase.index(userInputHeap)
-		if userInputHeap not in ascii_uppercase[:numberOfHeaps]:
-			raise LetterError
-		if userInputNumber > heaps[heapIndex]:
-			raise NumberError
-	except:
-		print "Enter a valid move. ( Heap, number )"
-		continue
-	heaps[heapIndex]-=userInputNumber
-	currentPlayer = not currentPlayer
-	checkWinState(currentPlayer)
+    displayHeap()
+    if currentPlayer:
+        print "Your move : ",
+    else:
+        computerMove()
+        currentPlayer = not currentPlayer
+        checkWinState(currentPlayer)
+        continue
+    try:
+        userInputHeap,userInputNumber = raw_input().split()
+        userInputNumber=int(userInputNumber)
+        heapIndex=ascii_uppercase.index(userInputHeap)
+        if userInputHeap not in ascii_uppercase[:numberOfHeaps]:
+            raise LetterError
+        if userInputNumber > heaps[heapIndex]:
+            raise NumberError
+    except:
+        print "Enter a valid move. ( Heap, number )"
+        continue
+    heaps[heapIndex]-=userInputNumber
+    currentPlayer = not currentPlayer
+    checkWinState(currentPlayer)
